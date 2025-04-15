efd9c191befd8e63c2559987b714f1b7058f43f6
Emerson Roncancio <roncancioavila@gmail.com>
2025-04-15 00:48:00 -0500

    validacion de queryregion

--- src/app/country/pages/by-country/by-country.component.ts
+++ src/app/country/pages/by-country/by-country.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, inject, resource, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, linkedSignal, resource, signal } from '@angular/core';
 import { InputSearchComponent } from "../../components/input-search/input-search.component";
 import { TableListComponent } from "../../components/table-list/table-list.component";
 import { CountryService } from '../../services/country.service';
-import { firstValueFrom, of } from 'rxjs';
-import { rxResource } from '@angular/core/rxjs-interop';
+import { firstValueFrom, map, of } from 'rxjs';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'by-country',
@@ -15,21 +16,25 @@ export class ByCountryComponent {
 
   countryService = inject(CountryService)
   placeholderSignal = signal<string>('Buscar por país');
-  countryList = signal('')
+  router = inject(Router)
 
-  // conutryResource = rxResource({
-  //   request: () => ({ country: this.countryList() }),
-  //   loader: ({ request }) => {
-  //     if(!request.country) return of([])
+  queryCountry = toSignal(
+    inject(ActivatedRoute).queryParams.pipe(
+      map(params => params['country'] || ''))
+  )
 
-  //     return this.countryService.searchByCountry(request.country)
-  //   }
-  // })
+  countryList = linkedSignal(() => this.queryCountry())
 
   conutryResource = resource({
     request: () => ({ country: this.countryList() }),
     loader: async ({ request }) => {
       if(!request.country) return []
+
+      this.router.navigate(['/country/by-country'], {
+        queryParams: {
+          country: request.country
+        }
+      })
 
       return await firstValueFrom(this.countryService.searchByCountry(request.country))
     }
--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -1,12 +1,21 @@
-import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, linkedSignal, signal } from '@angular/core';
 import { InputSearchComponent } from "../../components/input-search/input-search.component";
 import { TableListComponent } from "../../components/table-list/table-list.component";
 import { CountryService } from '../../services/country.service';
 import { Region } from '../../interfaces/by-region.interfaces';
 import { TopMenuComponent } from "../../components/top-menu/top-menu.component";
 import { TopRegionComponent } from "../../components/top-region/top-region.component";
-import { rxResource } from '@angular/core/rxjs-interop';
-import { of } from 'rxjs';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { map, of } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+
+
+const validateRegion = (region: string) => {
+  region = region.charAt(0).toUpperCase() + region.slice(1).toLowerCase()
+
+  const regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+  return regions.includes(region) ? region : 'Americas'
+}
 
 @Component({
   selector: 'by-region',
@@ -18,16 +27,25 @@ export class ByRegionComponent {
 
   regionList = signal('')
   contryService = inject(CountryService)
-  preregions = signal<Region | null>(null)
+  router = inject(Router)
 
-  // regionEffect = effect(()=>{
-  //   if(this.preregions() === null) return
-  //   this.contryService.searchByRegion(this.preregions()!)
-  // })
+  queryRegion = toSignal(
+    inject(ActivatedRoute).queryParams.pipe(
+      map(params => params['region'] || ''))
+    )
+
+  preregions = linkedSignal<Region | null>(() => validateRegion(this.queryRegion()) as Region)
+
   regionResource = rxResource({
     request: () => ({ region: this.preregions() }),
     loader: ({ request }) => {
       if(!request.region) return of([])
+
+      this.router.navigate(['/country/by-region'], {
+        queryParams: {
+          region: request.region
+        }
+      })
 
       return this.contryService.searchByRegion(request.region)
     }
